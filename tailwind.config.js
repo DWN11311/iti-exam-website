3e8eedd1534eb67e5cff28bbae8e0ad3f7f8717f
Ahmedkx <89226249+Ahmedkx@users.noreply.github.com>
2025-01-19 20:33:46 +0200

    feat: Add fail.html and update exam.html with new structure; modify input styles and adjust Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"], // Adjust the paths based on your project structure
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    // content: ["./src/**/*.{html,js}"], // Adjust the paths based on your project structure
+    content: ["./**/*.{html,js}"], // Adjust the paths based on your project structure
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
